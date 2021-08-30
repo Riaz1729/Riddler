74c163878dc23c004dc3c6b16a1f152086073de7
Riaz Helfer <rah2181@columbia.edu>
2021-08-29 23:33:36 -0400

    clean up

--- 8.26.2021/run.py
+++ 8.26.2021/run.py
@@ -1,44 +1,44 @@
 import math
 
+
 class Coordinate(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def __lt__(self, other):
-        return self.x < other.x or self.y<other.y
+        return self.x < other.x or self.y < other.y
 
     def get_magnitude(self):
-        return math.sqrt(self.x**2 + self.y**2)
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def __sub__(self, other):
-        return Coordinate(self.x - other.x, self.y-other.y)
+        return Coordinate(self.x - other.x, self.y - other.y)
 
     def __add__(self, other):
-        return Coordinate(self.x + other.x, self.y+other.y)
+        return Coordinate(self.x + other.x, self.y + other.y)
 
     def __truediv__(self, f):
-        return Coordinate(self.x/f, self.y/f)
+        return Coordinate(self.x / f, self.y / f)
 
     def __mul__(self, f):
-        return Coordinate(self.x*f, self.y*f)
+        return Coordinate(self.x * f, self.y * f)
 
     def __repr__(self):
         return f"({self.x}, {self.y})"
 
 
-
 def calc_x1(t):
-    return Coordinate(50, t*15)
+    return Coordinate(50, t * 15)
 
-def calc_v2(x1, x2 , s):
-    v = x2-x1
-    return v*s / v.get_magnitude()
-    
+
+def calc_v2(x1, x2, speed):
+    dir_vec = x2 - x1
+    return dir_vec * speed / dir_vec.get_magnitude()
+
 
 def calc_x2(v2, x2, step):
-    return x2 + v2*step
-
+    return x2 + v2 * step
 
 
 def check_convergence(step, speed):
@@ -52,10 +52,9 @@ def check_convergence(step, speed):
         x1 = calc_x1(t)
         x2 = calc_x2(v2, x2, step)
         v2 = calc_v2(x2, x1, speed)
-        #print(x1, x2)
+        # print(x1, x2)
 
     return x1.y < 100
-        
 
 
 def run():
@@ -63,16 +62,15 @@ def run():
     high = 30
     step = 0.001
 
-
     for i in range(100):
-        curr = .5*(high + low)
+        curr = 0.5 * (high + low)
         converges = check_convergence(step, curr)
         if converges:
             high = curr
         else:
             low = curr
 
-        print(curr, converges)
+    print(high)  # the last speed that converged
 
 
 run()
